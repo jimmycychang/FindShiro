ddb248a90c641a275b48647d0023e878220c88f5
Jimmy Chang <103914673+jimmycychang@users.noreply.github.com>
2023-06-09 23:21:34 -0400

    Update shiro.py

--- shiro.py
+++ shiro.py
@@ -1,21 +1,20 @@
 import pygame, random
+import base
 
 class SHIRO:
     def __init__(self):
-        self.cell_size = 40
-        self.cell_number = 20
-        self.x = random.randint(0, self.cell_number-1)
-        self.y = random.randint(0, self.cell_number-1)
-        self.screen = pygame.display.set_mode((self.cell_size*self.cell_number,self.cell_size*self.cell_number))
+        self.x = random.randint(0, base.cell_number-1)
+        self.y = random.randint(0, base.cell_number-1)
         self.pos = pygame.math.Vector2(self.x,self.y)
         self.shiro = pygame.image.load("images/shiro.png").convert_alpha()
 
     def draw_shiro(self):
-        shiro_rect = pygame.Rect(int(self.pos.x*self.cell_size),int(self.y*self.cell_size), self.cell_size, self.cell_size)
-        self.screen.blit(self.shiro, shiro_rect)
+        shiro_rect = pygame.Rect(int(self.pos.x*base.cell_size),int(self.y*base.cell_size), base.cell_size, base.cell_size)
+        base.screen.blit(self.shiro, shiro_rect)
 
     def randomize(self):
-        self.x = random.randint(0, self.cell_number-1)
-        self.y = random.randint(0, self.cell_number-1)
-        self.screen = pygame.display.set_mode((self.cell_size*self.cell_number,self.cell_size*self.cell_number))
+        self.x = random.randint(0, base.cell_number-1)
+        self.y = random.randint(0, base.cell_number-1)
         self.pos = pygame.math.Vector2(self.x,self.y)
+
+
