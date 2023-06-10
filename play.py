e1dbd7c05ee6e3c8317b9471c3ea23cab4faa955
Jimmy Chang <103914673+jimmycychang@users.noreply.github.com>
2023-06-09 23:22:25 -0400

    Update play.py

--- play.py
+++ play.py
@@ -10,7 +10,7 @@ game = MAIN_FUNC()
 clock = pygame.time.Clock()
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 150)
+pygame.time.set_timer(SCREEN_UPDATE, 100)
 
 while True:
     for event in pygame.event.get():
